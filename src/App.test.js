06e5ab2e447c6c7439667a35ac539280adaf105c
Ahmed ElSenousi <asenousy@MacBook-Pro-4.local>
2018-09-02 21:10:35 +0100

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { buildBlackList } from './helpers';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should', () => {
+  const users = [{
+    "name": "User",
+    "wont_eat": ["Fish"],
+    "drinks": ["Coke"]
+  }];
+  const venues = [{
+    "name": "Restaurant",
+    "food": ["Beef"],
+    "drinks": ["Coke"]
+  }];
+  expect(buildBlackList(users, venues)).toEqual([]);
 });
